d50eb1dfb9a625718f1acf4cd8ab3e801eba9c36
quasevi <willy.quasevi@gmail.com>
2021-12-05 21:05:04 +0100

    fix : secteur_id cannot be null

--- src/main/java/fixtures.java
+++ src/main/java/fixtures.java
@@ -9,7 +9,12 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class fixtures {
+
+
 
     public static void main(String[] args) {
 
@@ -18,8 +23,8 @@ public class fixtures {
         sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         Session session = sessionFactory.openSession();
 
-
-        String[] clearTables = {"Employes", "Secteur"};
+        // Clear table
+        String[] clearTables = {"Employes", "Secteur","Filliale","Entreprise"}; // programme idempotent
         for (String table : clearTables) {
             session.beginTransaction();
             String stringQuery = "DELETE FROM " + table;
@@ -30,48 +35,79 @@ public class fixtures {
 
         // create entreprise
         session.beginTransaction();
-        Entreprise iros = new Entreprise();
-        iros.setNom("Iros");
+        Entreprise nordBoucherie = new Entreprise();
+        nordBoucherie.setNom("nordBoucherie");
 
-        session.save(iros);
+        session.save(nordBoucherie);
+
+        Entreprise Total = new Entreprise();
+        Total.setNom("Total");
+        session.save(Total);
+
         session.getTransaction().commit();
 
-        //Create Secteur joke
+        //Create Secteur secteur 1 and 2
         session.beginTransaction();
-        Secteur joke = new Secteur();
-        joke.setNom("geo");
-        joke.setLocalisation("Paris");
-        session.save(joke);
+        Secteur secteur1 = new Secteur();
+        secteur1.setNom("Transport");
+        secteur1.setLocalisation("Lille");
+        session.save(secteur1);
+        session.getTransaction().commit();
+
+
+
+
+        session.beginTransaction();
+        Secteur secteur2 = new Secteur();
+        secteur2.setNom("Boucherie");
+        secteur2.setLocalisation("Paris");
+        session.save(secteur2);
         session.getTransaction().commit();
 
         //Create employes  1
         session.beginTransaction();
         Employes wil = new Employes();
-        wil.setNom("greg");
+        wil.setNom("wil");
         wil.setPrenom("juste ");
-        wil.setEmail("greg@gmail.com");
+        wil.setEmail("wil@gmail.com");
         wil.setAge(27);
         wil.setFonction("dev");
         wil.setTelephone("06 30 30 30 30");
         wil.setAdresse_postal("15 rue de la gare");
-        wil.setSecteur(joke);
+        wil.setSecteur(secteur2);
         session.save(wil);
         session.getTransaction().commit();
 
 
         // create fiffiale
         session.beginTransaction();
-        Filliale admin = new Filliale();
-        admin.setNom("wil");
-        admin.setNbre_employe(150);
+        Filliale filliale1 = new Filliale();
+        filliale1.setNom("filliale1");
+        filliale1.setNbre_employe(150);
 
-        admin.addSecteur(joke);
-        joke.addFilliale(admin);
+        Filliale filliale2 = new Filliale();
+        filliale2.setNom("filliale2");
+        filliale2.setNbre_employe(100);
 
-        admin.setEntreprise(iros);
+        filliale1.addSecteur(secteur2);
+        secteur2.addFilliale(filliale1);
+        filliale2.addSecteur(secteur2);
+        secteur2.addFilliale(filliale2);
 
-        session.save(admin);
-        session.save(joke);
+        filliale1.addSecteur(secteur1);
+        secteur1.addFilliale(filliale1);
+        filliale2.addSecteur(secteur1);
+        secteur1.addFilliale(filliale2);
+
+        filliale1.setEntreprise(nordBoucherie);
+        filliale2.setEntreprise(nordBoucherie);
+        filliale1.setEntreprise(Total);
+        filliale2.setEntreprise(Total);
+
+        session.save(filliale1);
+        session.save(filliale2);
+        session.save(secteur1);
+        session.save(secteur2);
         session.getTransaction().commit();
 
 
